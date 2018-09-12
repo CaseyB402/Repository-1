9543d317c14a90e58eabb088335785f960835f6e
Casey J Burgess <Adventureman402@gmail.com>
2018-09-12 17:16:21 -0600

    Added ground variables to C# script, and added ground code for jump controls to script.

--- DGM 1610_game/Assets/Scripts/CharacterMove.cs
+++ DGM 1610_game/Assets/Scripts/CharacterMove.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 	
+	// Player Movement Variables
 	public int MoveSpeed;
-	public float JumpHeight = 15;
+	public float JumpHeight;
+
+	// Player grounded variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	void FixedUpdate () {
+		Grounded = Physics20.OverlapCircle(GroundCheck.Position, GroundCheckRadius, WhatIsGround);
+	}
+
+
+
+
+	// Update Is Called Once Per Frame
 	void Update () {
+
+		// This Code Makes The Character Jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+			
+		}
+
 		print ("Hello World");	
+	}
+	public void Jump(){
+		GetComponent<Rigidbody2D>().Velocity = new Vector2(GetComponent<Rigidbody2D>().Velocity.x, JumpHeight);
+
+
 	}
 }
