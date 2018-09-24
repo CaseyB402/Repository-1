7d810d303eeb7d88a606e4890ff1886602e69918
Casey J Burgess <Adventureman402@gmail.com>
2018-09-24 17:20:20 -0600

    Added to move script to make PC double jump and stop sliding on ground. Added scoremananger script.

--- DGM 1610_game/Assets/Scripts/CharacterMove.cs
+++ DGM 1610_game/Assets/Scripts/CharacterMove.cs
@@ -7,11 +7,15 @@ public class CharacterMove : MonoBehaviour {
 		// Player Movement Variables
 	public int MoveSpeed;
 	public float JumpHeight;
+	private bool DoubleJump;
  	// Player grounded variables
 	public Transform GroundCheck;
 	public float GroundCheckRadius;
 	public LayerMask WhatIsGround;
 	private bool Grounded;
+
+	//Non Stick Player
+	private float MoveVelocity;
  	// Use this for initialization
 	void Start () {
 		print("Hello World!");
@@ -28,15 +32,27 @@ public class CharacterMove : MonoBehaviour {
 		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
 			Jump();
 		}
+		//Double Jump Code
+		if(Grounded)
+			DoubleJump = false;
+			if(Input.GetKeyDown(KeyCode.Space)&& !DoubleJump && !Grounded){
+				Jump();
+				DoubleJump = true;
+			}
+			//Non Stick Player
+			MoveVelocity = 0f;
  		// This code makes the character move from side to side using the A&D keys
 		if(Input.GetKey (KeyCode.D)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = MoveSpeed;
 			
 		}
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			//GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			MoveVelocity = -MoveSpeed;
 			
 		}
+		GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
  	}
  	public void Jump(){
 		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
